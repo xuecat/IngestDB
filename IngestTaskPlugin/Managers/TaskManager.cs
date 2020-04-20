d9fc7e314aa70c5d5d2fe851a9c1bbce43a33f2a
houkunhai <houkunhai@sobey.com>
2020-04-20 10:03:39 +0800

    明确CooperantType引用

--- IngestTaskPlugin/Managers/TaskManager.cs
+++ IngestTaskPlugin/Managers/TaskManager.cs
@@ -18,6 +18,7 @@ using IngestTaskPlugin.Models;
 using TaskInfoRequest = IngestTaskPlugin.Dto.TaskInfoResponse;
 using TaskContentRequest = IngestTaskPlugin.Dto.TaskContentResponse;
 using TaskInfoRescheduledRequest = IngestTaskPlugin.Dto.Response.TaskInfoRescheduledResponse;
+using CooperantType = IngestTaskPlugin.Dto.CooperantType;
 
 namespace IngestTaskPlugin.Managers
 {
