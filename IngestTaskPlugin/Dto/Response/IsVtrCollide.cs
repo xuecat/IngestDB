88078012c3b2adbbba91066862024ba1c58bb062
wangqiu <wangqiu@sobey.com>
2020-04-23 17:02:27 +0800

    修改新接口注释

--- IngestTaskPlugin/Dto/Response/IsVtrCollide.cs
+++ IngestTaskPlugin/Dto/Response/IsVtrCollide.cs
@@ -6,7 +6,9 @@ namespace IngestTaskPlugin.Dto
 {
     public class IsVtrCollide
     {
+        /// <summary>暂时无</summary>
         public VTRCollideResult Result { get; set; }
+        /// <summary>暂时无</summary>
         public TaskContentResponse CollideTaskContent { get; set; }
     }
 }
