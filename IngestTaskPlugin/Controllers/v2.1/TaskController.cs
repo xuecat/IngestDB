6c1fa8d760a02ab3e8683047f247fb7bcf716e70
wangqiu <wangqiu@sobey.com>
2020-12-16 17:17:46 +0800

    修改点接口

--- IngestTaskPlugin/Controllers/v2.1/TaskController.cs
+++ IngestTaskPlugin/Controllers/v2.1/TaskController.cs
@@ -84,6 +84,53 @@ namespace IngestTaskPlugin.Controllers.v2._1
         }
 
         /// <summary>
+        /// 获取任务存数据库db对应的数据
+        /// </summary>
+        /// <remarks>
+        /// 例子:
+        ///
+        /// </remarks>
+        /// <param name="taskid">任务id，</param>
+        /// <returns>任务内容全部信息包含元数据</returns>
+        [HttpGet("db/{taskid}")]
+        [ApiExplorerSettings(GroupName = "v2.1")]
+        public async Task<ResponseMessage<Models.DbpTask>> GetTaskDBInfoByID([FromRoute, BindRequired]int taskid)
+        {
+            var Response = new ResponseMessage<Models.DbpTask>();
+            if (taskid <= 0)
+            {
+                Response.Code = ResponseCodeDefines.ModelStateInvalid;
+                Response.Msg = "request param error";
+            }
+
+            try
+            {
+                Response.Ext = await _taskManage.GetTaskInfoByID<Models.DbpTask>(taskid, 0);
+                if (Response.Ext == null)
+                {
+                    Response.Code = ResponseCodeDefines.NotFound;
+                    Response.Msg = $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.FullName}:error info: not find data!";
+                }
+            }
+            catch (Exception e)
+            {
+                if (e.GetType() == typeof(SobeyRecException))//sobeyexcep会自动打印错误
+                {
+                    SobeyRecException se = e as SobeyRecException;
+                    Response.Code = se.ErrorCode.ToString();
+                    Response.Msg = se.Message;
+                }
+                else
+                {
+                    Response.Code = ResponseCodeDefines.ServiceError;
+                    Response.Msg = "GetTaskDBInfoByID 21 error info:" + e.Message;
+                    Logger.Error(Response.Msg);
+                }
+            }
+            return Response;
+        }
+
+        /// <summary>
         /// 根据以前任务添加重调度任务, 一般任务调度失败，开始msv失败才会调，这样产生新的任务重新开始采集
         /// </summary>
         /// <remarks>
